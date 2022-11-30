5c6eedb36796324921cb93538dd20d0cec489c91
Amador Aguilar <90819559+devAmador91@users.noreply.github.com>
2022-11-30 23:19:32 +0000

    Update config.js

--- database/config.js
+++ database/config.js
@@ -1,29 +1,33 @@
-// NOTA POR FAVOR NO MODIFICAR ABSOLUTAMENTE NADA
+require('dotenv').config(); 
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT} = process.env;
+const { Sequelize } = require('sequelize');
 
-const { Sequelize } = require("sequelize");
-
-const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
-const conexion_local = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
-const conexion_remota = process.env.DATABASE_URL;
-
-const options_local = {
-   logging: false,
-   native: false
-}
-
-const options_remoto = {
-   logging: false,
-   native: false,
-   dialectOptions: {
+const sequelize = process.env.NODE_ENV === "production"
+? new Sequelize({
+    database: DB_NAME,
+    dialect: "postgres",
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USER,
+    password: DB_PASSWORD,
+    pool: {
+      max: 3,
+      min: 1,
+      idle: 10000,
+    },
+    dialectOptions: {
       ssl: {
-      require: true,
-      rejectUnauthorized: false,
-     },
-  },
-}
-
-const sequelize = new Sequelize(
-   conexion_remota || conexion_local,
-   conexion_remota ? options_remoto : options_local);
+        require: true,
+    
+        rejectUnauthorized: false,
+      },
+      keepAlive: true,
+    },
+    ssl: true,
+  })
+: new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
 
 module.exports = sequelize;
